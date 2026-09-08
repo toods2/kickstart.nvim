0806c26228c9ff4679999198a6ad2da4b66cc7c6
Toods2 <toods2@gmail.com>
2026-09-08 17:28:45 -0500

    fix botched rebase

--- init.lua
+++ init.lua
@@ -1035,7 +1035,6 @@ do
   -- require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
   -- require 'kickstart.plugins.neo-tree'
-  require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
   --
